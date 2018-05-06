d5e204ab727f427df8cfb7d47ab1200f9737b98d
Tapio Löytty <tapsa@orange-media.fi>
2018-05-06 20:11:42 +0300

    Initial

---
+++ instagramapi-callback.php
@@ -0,0 +1,14 @@
+<?php 
+
+$module_name = "InstagramAPI";
+$mod = $modules->get($module_name);
+
+if(isset($_REQUEST['code'])) {
+	if($token = $mod->getOAuthToken($_REQUEST['code'], true))	
+		$cache->save($mod::token, $token, WireCache::expireNever);	
+}
+
+// URL is hard coded, i know, it's a bit shitty wayt, but neither does PW provide module edit URL from API level!!!
+$session->redirect($config->urls->root . "admin/module/edit?name=$module_name&collapse_info=1");
+
+return $this->halt();
