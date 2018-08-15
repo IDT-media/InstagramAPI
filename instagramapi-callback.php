f7dcd6a81b0f93ffe8565d40c64ca2a5a907ada8
stikki <tapio.loytty@gmail.com>
2018-08-15 20:13:07 +0300

    Updating callback template

--- instagramapi-callback.php
+++ instagramapi-callback.php
@@ -4,8 +4,13 @@ $module_name = "InstagramAPI";
 $mod = $modules->get($module_name);
 
 if(isset($_REQUEST['code'])) {
-	if($token = $mod->getOAuthToken($_REQUEST['code'], true))	
-		$cache->save($mod::token, $token, WireCache::expireNever);	
+	if($data = $mod->getOAuthToken($_REQUEST['code'])) {	
+
+		$tokens = $cache->get($mod::token);
+		$tokens[$data->user->id] = $data->access_token;
+
+		$cache->save($mod::token, $tokens, WireCache::expireNever);	
+	}
 }
 
 // URL is hard coded, i know, it's a bit shitty wayt, but neither does PW provide module edit URL from API level!!!
